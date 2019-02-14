68497fa9b92c18f9a5ac79bd8aabc9bc62145a63
nadeem <nadeem.hisham@gmail.com>
2019-02-14 23:59:02 +0200

    Milestone 1

--- app/src/main/java/com/example/thermonitorbeta/Main2Activity.java
+++ app/src/main/java/com/example/thermonitorbeta/Main2Activity.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+
 
 public class Main2Activity extends AppCompatActivity {
 
     ListView listView;
 
+    int[] Images = {R.drawable.iphone, R.drawable.blackberry, R.drawable.linux, R.drawable.android, R.drawable.windows};
+
+    String[] Name = {"iPhone", "BlackBerry", "Linux", "Android", "Windows"};
+
+        CustomAdapter customAdapter = new CustomAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(customAdapter);
+
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(Main2Activity.this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Static_list));
 
@@ -25,12 +39,45 @@ public class Main2Activity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 Intent int2 = new Intent(Main2Activity.this, Main3Activity.class);
-                int2.putExtra("OStype",listView.getItemAtPosition(position).toString());
+
                 startActivity(int2);
             }
         });
 
-        listView.setAdapter(adapter);
+
+    }
+
+    class CustomAdapter extends BaseAdapter
+    {
+
+        @Override
+        public int getCount() {
+            return Images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view = getLayoutInflater().inflate(R.layout.customizedlayout, null);
+            ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+            TextView textView = (TextView) view.findViewById(R.id.textView);
+
+            imageView.setImageResource(Images[position]);
+            textView.setText(Name[position]);
+
+            return view;
+        }
     }
 }
--- app/src/main/java/com/example/thermonitorbeta/MainActivity.java
+++ app/src/main/java/com/example/thermonitorbeta/MainActivity.java
@@ -1,41 +1,140 @@
 package com.example.thermonitorbeta;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public Button button;
     public Toolbar toolbar;
     public EditText email;
     public EditText password;
+    public Button noAccButton;
+    EditText usernameText;
+    EditText passwordText;
+    EditText emailText;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
-    public void activateButton()
-    {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent open = new Intent(MainActivity.this, Main2Activity.class);
-                startActivity(open);
-            }
-        });
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        email = (EditText)findViewById(R.id.email);
+        email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
-        button = (Button)findViewById(R.id.button1);
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        button = (Button) findViewById(R.id.button1);
+        noAccButton = (Button) findViewById(R.id.noAccountButton);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(getResources().getString(R.string.app_name));
-        activateButton();
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.noAccountButton).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
+
+        usernameText = (EditText) findViewById(R.id.userName);
+        passwordText = (EditText) findViewById(R.id.passWord);
+        emailText = (EditText) findViewById(R.id.eMail);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        userLogin();
+
+
+
+    }
+
+    public void userLogin()
+    {
+        String Username = usernameText.getText().toString().trim();
+        String Password = passwordText.getText().toString().trim();
+        String Email = emailText.getText().toString().trim();
+
+        if(Username.isEmpty())
+        {
+            usernameText.setError("Username is required");
+            usernameText.requestFocus();
+            return;
+        }
+
+        if (Password.isEmpty())
+        {
+            passwordText.setError("Password is required");
+            passwordText.requestFocus();
+            return;
+        }
+
+        if (Email.isEmpty())
+        {
+            emailText.setError("Email is required");
+            emailText.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches())
+        {
+            emailText.setError("The entered email is invalid");
+            emailText.requestFocus();
+            return;
+        }
+
+        if (Password.length()<6)
+        {
+            passwordText.setError("Password must be at least 6 characters long");
+            passwordText.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful())
+                {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.noAccountButton:
+                Intent noAcc = new Intent(this, RegisterActivity.class);
+                startActivity(noAcc);
+                break;
+
+            case R.id.button1:
+                userLogin();
+                break;
+        }
+
     }
 }
